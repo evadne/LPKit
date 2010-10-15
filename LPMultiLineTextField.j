evadne/LPKit

commit 83406ffc9c9977a9ccd89474fda936a1e1a15497

fixes circumstance where a non-editable multiline text field’s value is erroneously determined by its placeholder value

LPMultiLineTextField.j
@@ -80,6 +80,7 @@ var CPTextFieldInputOwner = nil;
 
 - (void)layoutSubviews
 {
+
     [super layoutSubviews];
     
     
@@ -103,7 +104,7 @@ var CPTextFieldInputOwner = nil;
     DOMElement.style.color = [[self currentValueForThemeAttribute:@"text-color"] cssString];
     DOMElement.style.font = [[self currentValueForThemeAttribute:@"font"] cssString];
 
-    if ([self hasThemeState:CPTextFieldStatePlaceholder]) {
+    if ([self hasThemeState:CPTextFieldStatePlaceholder] && [self isEditable]) {
     
     	DOMElement.value = _placeholderString;
     
@@ -209,7 +210,10 @@ var CPTextFieldInputOwner = nil;
 - (CPString)stringValue
 {
 
-    if ([self hasThemeState:CPTextFieldStatePlaceholder]) return @"";
+    if ([self hasThemeState:CPTextFieldStatePlaceholder])
+    if (!_DOMTextareaElement)
+    return nil;
+
     return (!!_DOMTextareaElement) ? _DOMTextareaElement.value : @"";
 }
 

commit 32da1dfae4e63dbb3ee40c1291fe956e5431de5f

fixes bug where the placeholder gets carried over because of an invisible-then regression in 83406ffc9c9977a9ccd89474fda936a1e1a15497.  revises the handling of object values and string values and conforms it to CPTextField for easy bindings support in the future

LPMultiLineTextField.j
@@ -104,13 +104,13 @@ var CPTextFieldInputOwner = nil;
     DOMElement.style.color = [[self currentValueForThemeAttribute:@"text-color"] cssString];
     DOMElement.style.font = [[self currentValueForThemeAttribute:@"font"] cssString];
 
-    if ([self hasThemeState:CPTextFieldStatePlaceholder] && [self isEditable]) {
+    if ([self hasThemeState:CPTextFieldStatePlaceholder]) {
     
-    	DOMElement.value = _placeholderString;
+    	DOMElement.value = [self placeholderString];
     
     } else {
 
-        DOMElement.value = _stringValue || @"";
+        DOMElement.value = [self stringValue];
     
     }
 }
@@ -145,10 +145,12 @@ var CPTextFieldInputOwner = nil;
 
 - (void)keyUp:(CPEvent)anEvent
 {
-    if (_stringValue !== [self stringValue])
+    var oldStringValue = [self stringValue];
+    [self _setStringValue:[self _DOMTextareaElement].value];
+
+    if (oldStringValue !== [self stringValue])
     {
-        _stringValue = [self stringValue];
-        
+                
         if (!_isEditing)
         {
             _isEditing = YES;
@@ -207,21 +209,23 @@ var CPTextFieldInputOwner = nil;
     return YES;
 }
 
-- (CPString)stringValue
+- (void)_setStringValue:(id)aValue
 {
-
-    if ([self hasThemeState:CPTextFieldStatePlaceholder])
-    if (!_DOMTextareaElement)
-    return nil;
-
-    return (!!_DOMTextareaElement) ? _DOMTextareaElement.value : @"";
+    [self willChangeValueForKey:@"objectValue"];
+    [super setObjectValue:String(aValue)];
+    [self _updatePlaceholderState];
+    [self didChangeValueForKey:@"objectValue"];
 }
 
-- (void)setStringValue:(CPString)aString
+- (void)setObjectValue:(id)aValue
 {
-    _stringValue = aString;
+
+    [super setObjectValue:aValue];
+
+	if (CPTextFieldInputOwner === self || [[self window] firstResponder] === self)
+        [self _DOMTextareaElement].value = aValue;
+
     [self _updatePlaceholderState];
-    [self setNeedsLayout];
 }
 
 @end